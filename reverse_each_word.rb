3808b40eda3087546635ff7e88d644bea30aa35a
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-05-31 16:13:32 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+# def reverse_each_word(sentence1)
+ 
+# end
+  
+def reverse_each_word(sentence)
+  sentence_array = sentence.split
+  
+  reversed_sentence = " "
+ 
+  sentence_array.each do|word| 
+    reversed_word = word.reverse
+    reversed_sentence = "#{reversed_sentence} #{reversed_word}"
+   #reversed_sentence = reversed_sentence + " " + reversed_word 
+   end
+ reversed_sentence
+end
